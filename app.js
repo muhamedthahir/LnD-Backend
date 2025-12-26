2183872608984eee752628876b6c32300136ba78
Muhammed Salman S F <muhammedsalman.techtutor@gmail.com>
2025-12-26 15:17:17 +0530

    uth

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
   }
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, Pragma');
   
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
